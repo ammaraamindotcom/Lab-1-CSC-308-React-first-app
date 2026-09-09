5614f274baa4b4e5b7c8840ee9f7dbcaf9597e4c
ammaraamindotcom <ammaraamin4@gmail.com>
2026-09-08 21:52:54 -0700

    Show user ID in table

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -5,8 +5,9 @@ function TableHeader() {
     return (
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Job</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Job</th>
         </tr>
       </thead>
     );
@@ -16,6 +17,7 @@ function TableHeader() {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
+          <td>{row.id}</td>
           <td>{row.name}</td>
           <td>{row.job}</td>
           <td>
